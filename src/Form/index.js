0308916c8454e2432b79494abd63013df647ab74
Piotr Paczuski <piotr-paczuski@o2.pl>
2022-11-28 05:11:57 +0100

    changed function names

--- src/Form/index.js
+++ src/Form/index.js
@@ -9,17 +9,17 @@ const Form = () => {
     const [inputValue, setInputValue] = useState("");
     const [inputCurrency, setInputCurrency] = useState("PLN");
     const [outputCurrency, setOutputCurrency] = useState("EUR");
-    const [valueCurrentRate, setValuecurrentRate] = useState("Obecny kurs wynosi: 0.21")
+    const [valueCurrentRate, setValueCurrentRate] = useState("Obecny kurs wynosi: 0.21")
     const [valueElement, setValueElement] = useState(" N/A");
     const [valueCurrency, setValueCurrency] = useState("");
 
-    const xyz = {
+    const setResults = {
         currencyFrom: "",
         currencyTo: "",
         resultText: "",
-        setResultText(currencyFrom, currencyTo, resultText) {
+        setCurrentRate(currencyFrom, currencyTo, resultText) {
             if (currencyFrom === `${this.currencyFrom}` && currencyTo === `${this.currencyTo}`) {
-                setValuecurrentRate(resultText);
+                setValueCurrentRate(resultText);
             }
         },
         setResult(currencyFrom, currencyTo, rate, currency) {
@@ -33,18 +33,18 @@ const Form = () => {
         setInputCurrency(currencyFrom)
         setOutputCurrency(currencyTo)
 
-        xyz.currencyFrom = currencyFrom;
-        xyz.currencyTo = currencyTo;
+        setResults.currencyFrom = currencyFrom;
+        setResults.currencyTo = currencyTo;
 
-        xyz.setResultText("PLN", "PLN", "Wybrałeś te same waluty!")
-        xyz.setResultText("PLN", "EUR", "Obecny kurs wynosi: 0.21")
-        xyz.setResultText("PLN", "USD", "Obecny kurs wynosi: 0.21")
-        xyz.setResultText("EUR", "PLN", "Obecny kurs wynosi: 4.77")
-        xyz.setResultText("EUR", "EUR", "Wybrałeś te same waluty!")
-        xyz.setResultText("EUR", "USD", "Obecny kurs wynosi: 0.98")
-        xyz.setResultText("USD", "PLN", "Obecny kurs wynosi: 4.88")
-        xyz.setResultText("USD", "EUR", "Obecny kurs wynosi: 1.02")
-        xyz.setResultText("USD", "USD", "Wybrałeś te same waluty!")
+        setResults.setCurrentRate("PLN", "PLN", "Wybrałeś te same waluty!")
+        setResults.setCurrentRate("PLN", "EUR", "Obecny kurs wynosi: 0.21")
+        setResults.setCurrentRate("PLN", "USD", "Obecny kurs wynosi: 0.21")
+        setResults.setCurrentRate("EUR", "PLN", "Obecny kurs wynosi: 4.77")
+        setResults.setCurrentRate("EUR", "EUR", "Wybrałeś te same waluty!")
+        setResults.setCurrentRate("EUR", "USD", "Obecny kurs wynosi: 0.98")
+        setResults.setCurrentRate("USD", "PLN", "Obecny kurs wynosi: 4.88")
+        setResults.setCurrentRate("USD", "EUR", "Obecny kurs wynosi: 1.02")
+        setResults.setCurrentRate("USD", "USD", "Wybrałeś te same waluty!")
     }
 
     const calculateCurrency = (rate, value) => {
@@ -55,18 +55,18 @@ const Form = () => {
     const onFormSubmit = (event) => {
         event.preventDefault();
 
-        xyz.currencyFrom = inputCurrency;
-        xyz.currencyTo = outputCurrency;
+        setResults.currencyFrom = inputCurrency;
+        setResults.currencyTo = outputCurrency;
 
-        xyz.setResult("PLN", "PLN", 1, " PLN")
-        xyz.setResult("PLN", "EUR", 0.21, " EUR")
-        xyz.setResult("PLN", "USD", 0.21, " USD")
-        xyz.setResult("EUR", "PLN", 4.77, " PLN")
-        xyz.setResult("EUR", "EUR", 1, " EUR")
-        xyz.setResult("EUR", "USD", 0.98, " USD")
-        xyz.setResult("USD", "PLN", 4.88, " PLN")
-        xyz.setResult("USD", "EUR", 1.02, " EUR")
-        xyz.setResult("USD", "USD", 1, " USD")
+        setResults.setResult("PLN", "PLN", 1, " PLN")
+        setResults.setResult("PLN", "EUR", 0.21, " EUR")
+        setResults.setResult("PLN", "USD", 0.21, " USD")
+        setResults.setResult("EUR", "PLN", 4.77, " PLN")
+        setResults.setResult("EUR", "EUR", 1, " EUR")
+        setResults.setResult("EUR", "USD", 0.98, " USD")
+        setResults.setResult("USD", "PLN", 4.88, " PLN")
+        setResults.setResult("USD", "EUR", 1.02, " EUR")
+        setResults.setResult("USD", "USD", 1, " USD")
     }
 
     return (
