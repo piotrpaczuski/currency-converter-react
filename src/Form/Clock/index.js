27635f7e52b8389cdb49cfd3078862b062fbb643
Piotr Paczuski <piotr-paczuski@o2.pl>
2022-12-27 22:02:39 +0100

    optimilized clock code

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -3,35 +3,11 @@ import "./style.css";
 
 const ActuallyDate = () => {
 
-    const [actuallyDate, setActuallyDate] = useState({
-        time: new Date().toLocaleDateString(
-            undefined,
-            {
-                weekday: "long",
-                day: "numeric",
-                month: "long",
-                hour: "numeric",
-                minute: "numeric",
-                second: "numeric"
-            }
-        )
-    });
+    const [actuallyDate, setActuallyDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            let date = new Date().toLocaleDateString(
-                undefined,
-                {
-                    weekday: "long",
-                    day: "numeric",
-                    month: "long",
-                    hour: "numeric",
-                    minute: "numeric",
-                    second: "numeric"
-                }
-            );
-
-            setActuallyDate({ time: date });
+            setActuallyDate(new Date());
         }, 1000);
 
         return () => {
@@ -39,8 +15,20 @@ const ActuallyDate = () => {
         };
     }, []);
 
+    const formattedDate = actuallyDate.toLocaleDateString(
+        undefined,
+        {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric"
+        }
+    );
+
     return (
-        <p className="form__date">Dzisiaj jest {actuallyDate.time}</p>
+        <p className="form__date">Dzisiaj jest {formattedDate}</p>
     )
 }
 
